eb611571febca12a99bbf109ce1b977ac4c27ea8
Mark Hsu <mark50404@gmail.com>
2019-09-11 16:51:55 +0800

    completed 10041

--- 1star/10041.cpp
+++ 1star/10041.cpp
@@ -1,7 +1,28 @@
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
 int main() {
-    
+    int r, s[500];
+    cin >> r;
+
+    for (int k = 0; k < r; k++) {
+        int n, sum = 0, home;
+        cin >> n;
+
+        for (int i = 0; i < n; i++) {
+            cin >> s[i];
+        }
+
+        sort(s, s + n);
+
+        home = s[n / 2];
+        sum = 0;
+        for (int j = 0; j < n; j++) {
+            sum += abs(s[j] - home);
+        }
+
+        cout << sum << endl;
+    }
 }
