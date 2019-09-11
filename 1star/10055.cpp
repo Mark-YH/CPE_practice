4ca686d6dd9f252293a554ff703a6dab33c237f9
Mark Hsu <mark50404@gmail.com>
2019-09-11 17:01:19 +0800

    completed 10055

---
+++ 1star/10055.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Mark Hsu on 2019-09-11.
+//
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    long long int a, b;
+    while (cin >> a >> b) {
+        cout << abs(a - b) << endl;
+    }
+}
